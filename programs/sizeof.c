9dc2b32dee5e8becc7bc88c02657f8231fa0c4e5
uday4a9 <uday4a9@gmail.com>
2016-11-17 12:05:42 +0530

    few cleanups in C repo

--- programs/sizeof.c
+++ programs/sizeof.c
@@ -3,12 +3,19 @@
 #include <string.h>
 
 #define SIZEOF(X) ((char*)(&x + 1) - (char*)(&X))
+#if 0
+    #undef SIZEOF
+    #define SIZEOF(X) ((&x + 1) - (&X))
+#endif
 
 int main()
 {
     short x;
+    int arr[5];
 
-    printf("size : %ld\n", SIZEOF(x));
+    //printf("size : %ld\n", SIZEOF(x));
+    //printf("sz : %d\n", (&x+1) - (&x));
+    printf("diff : %ld\n", (char*)(&arr[0]+1) - (char*)&arr[0]);
 
     return 0;
 }
